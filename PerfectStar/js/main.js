dc89e94108a1f696ca970662b7d13d2754434472
Ariel <johnarielyap143@gmail.com>
2025-04-25 17:01:09 +0800

    perfect star

--- PerfectStar/js/main.js
+++ PerfectStar/js/main.js
@@ -1,132 +1,331 @@
-var navbar = document.querySelector(".navbar");
-window.onscroll = () => {
-  this.scrollY > 20
-    ? navbar.classList.add("sticky")
-    : navbar.classList.remove("sticky");
-};
+/* =====================================
+Template Name: 	Mediplus.
+Author Name: Naimur Rahman
+Website: http://wpthemesgrid.com/
+Description: Mediplus - Doctor HTML Template.
+Version:	1.1
+========================================*/   
+/*=======================================
+[Start Activation Code]
+=========================================
+* Sticky Header JS
+* Search JS
+* Mobile Menu JS
+* Hero Slider JS
+* Testimonial Slider JS
+* Portfolio Slider JS
+* Clients Slider JS
+* Single Portfolio Slider JS
+* Accordion JS
+* Nice Select JS
+* Date Picker JS
+* Counter Up JS
+* Checkbox JS
+* Right Bar JS
+* Video Popup JS
+* Wow JS
+* Scroll Up JS
+* Animate Scroll JS
+* Stellar JS
+* Google Maps JS
+* Preloader JS
+=========================================
+[End Activation Code]
+=========================================*/ 
+(function($) {
+    "use strict";
+     $(document).on('ready', function() {
+	
+        jQuery(window).on('scroll', function() {
+			if ($(this).scrollTop() > 200) {
+				$('#header .header-inner').addClass("sticky");
+			} else {
+				$('#header .header-inner').removeClass("sticky");
+			}
+		});
+		
+		/*====================================
+			Sticky Header JS
+		======================================*/ 
+		jQuery(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('.header').addClass("sticky");
+			} else {
+				$('.header').removeClass("sticky");
+			}
+		});
+		
+		$('.pro-features .get-pro').on( "click", function(){
+			$('.pro-features').toggleClass('active');
+		});
+		
+		/*====================================
+			Search JS
+		======================================*/ 
+		$('.search a').on( "click", function(){
+			$('.search-top').toggleClass('active');
+		});
+		
+		/*====================================
+			Mobile Menu
+		======================================*/ 	
+		$('.menu').slicknav({
+			prependTo:".mobile-nav",
+			duration: 300,
+			closeOnClick:true,
+		});
+		
+		/*===============================
+			Hero Slider JS
+		=================================*/ 
+		$(".hero-slider").owlCarousel({
+			loop:true,
+			autoplay:true,
+			smartSpeed: 500,
+			autoplayTimeout:3500,
+			singleItem: true,
+			autoplayHoverPause:true,
+			items:1,
+			nav:true,
+			navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+			dots:false,
+		});
 
-const navMenu = document.querySelector(".menu");
-navToggle = document.querySelector(".menu-btn");
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
-}
+		/*===============================
+			Testimonial Slider JS
+		=================================*/ 
+		$('.testimonial-slider').owlCarousel({
+			items:3,
+			autoplay:true,
+			autoplayTimeout:4500,
+			smartSpeed:300,
+			autoplayHoverPause:true,
+			loop:true,
+			merge:true,
+			nav:false,
+			dots:true,
+			responsive:{
+				1: {
+					items:1,
+				},
+				300: {
+					items:1,
+				},
+				480: {
+					items:1,
+				},
+				768: {
+					items:2,
+				},
+				1170: {
+					items:3,
+				},
+			}
+		});
+		
+		/*===============================
+			Portfolio Slider JS
+		=================================*/ 
+		$('.portfolio-slider').owlCarousel({
+			autoplay:true,
+			autoplayTimeout:4000,
+			margin:15,
+			smartSpeed:300,
+			autoplayHoverPause:true,
+			loop:true,
+			nav:true,
+			dots:false,
+			responsive:{
+				300: {
+					items:1,
+				},
+				480: {
+					items:2,
+				},
+				768: {
+					items:2,
+				},
+				1170: {
+					items:4,
+				},
+			}
+		});
+		
+		/*=====================================
+			Counter Up JS
+		======================================*/
+		$('.counter').counterUp({
+			delay:20,
+			time:2000
+		});
+		
+		/*===============================
+			Clients Slider JS
+		=================================*/ 
+		$('.clients-slider').owlCarousel({
+			items:5,
+			autoplay:true,
+			autoplayTimeout:3500,
+			margin:15,
+			smartSpeed: 400,
+			autoplayHoverPause:true,
+			loop:true,
+			nav:false,
+			dots:false,
+			responsive:{
+				300: {
+					items:1,
+				},
+				480: {
+					items:2,
+				},
+				768: {
+					items:3,
+				},
+				1170: {
+					items:5,
+				},
+			}
+		});
+		
+		/*====================================
+			Single Portfolio Slider JS
+		======================================*/ 
+		$('.pf-details-slider').owlCarousel({
+			items:1,
+			autoplay:false,
+			autoplayTimeout:5000,
+			smartSpeed: 400,
+			autoplayHoverPause:true,
+			loop:true,
+			merge:true,
+			nav:true,
+			dots:false,
+			navText: ['<i class="icofont-rounded-left"></i>', '<i class="icofont-rounded-right"></i>'],
+		});
+		
+		/*===================
+			Accordion JS
+		=====================*/ 
+		$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+		$('.accordion a').on('click', function(j) {
+			var dropDown = $(this).closest('li').find('p');
+			$(this).closest('.accordion').find('p').not(dropDown).slideUp(300);
+			if ($(this).hasClass('active')) {
+				$(this).removeClass('active');
+			} else {
+				$(this).closest('.accordion').find('a.active').removeClass('active');
+				$(this).addClass('active');
+			}
+			dropDown.stop(false, true).slideToggle(300);
+			j.preventDefault();
+		});
+		
+		/*====================================
+			Nice Select JS
+		======================================*/ 	
+		$('select').niceSelect();
+		
+		/*=====================================
+			Date Picker JS
+		======================================*/ 
+		$( function() {
+			$( "#datepicker" ).datepicker();
+		} );
+		
+		
+		
+		/*===============================
+			Checkbox JS
+		=================================*/  
+		$('input[type="checkbox"]').change(function(){
+			if($(this).is(':checked')){
+				$(this).parent("label").addClass("checked");
+			} else {
+				$(this).parent("label").removeClass("checked");
+			}
+		});
+		
+		/*===============================
+			Right Bar JS
+		=================================*/ 
+		$('.right-bar .bar').on( "click", function(){
+			$('.sidebar-menu').addClass('active');
+		});
+		$('.sidebar-menu .cross').on( "click", function(){
+			$('.sidebar-menu').removeClass('active');
+		});
+		
+		/*=====================
+			Video Popup JS
+		=======================*/ 
+		$('.video-popup').magnificPopup({
+			type: 'video',	
+		});
+		
+		/*================
+			Wow JS
+		==================*/		
+		var window_width = $(window).width();   
+			if(window_width > 767){
+            new WOW().init();
+		}
+	
+		/*===================
+			Scroll Up JS
+		=====================*/
+		$.scrollUp({
+			scrollText: '<span><i class="fa fa-angle-up"></i></span>',
+			easingType: 'easeInOutExpo',
+			scrollSpeed: 900,
+			animation: 'fade'
+		}); 
 
-const navLink = document.querySelectorAll(".nav-link");
-function linkAction() {
-  const navMenu = document.querySelector(".menu");
-  navMenu.classList.remove("active");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+		/*=======================
+			Animate Scroll JS
+		=========================*/
+		$('.scroll').on("click", function (e) {
+			var anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top - 100
+				}, 1000);
+			e.preventDefault();
+		});
+		
+		/*=======================
+			Stellar JS
+		=========================*/
+		$.stellar({
+		  horizontalOffset: 0,
+		  verticalOffset: 0
+		});
 
-const Section = document.querySelectorAll("section[id]");
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-  Section.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".links a[href*=" + sectionId + "]")
-        .classList.add("active");
-    } else {
-      document
-        .querySelector(".links a[href*=" + sectionId + "]")
-        .classList.remove("active");
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
-
-const skills_wrap = document.querySelector(".about-skills"),
-  skills_bar = document.querySelectorAll(".progress-line");
-window.addEventListener("scroll", () => {
-  skillsEffect();
-});
-
-function checkScroll(el) {
-  let rect = el.getBoundingClientRect();
-
-  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
-  return false;
-}
-function skillsEffect() {
-  if (!checkScroll(skills_wrap)) return;
-  skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
-}
-
-const FilterContainer = document.querySelector(".portfolio-filter"),
-  filterBtns = FilterContainer.children;
-totalFilterBtn = filterBtns.length;
-(PortfolioItems = document.querySelectorAll(".portfolio-item")),
-  (totalportfolioItem = PortfolioItems.length);
-for (let i = 0; i < totalFilterBtn; i++) {
-  filterBtns[i].addEventListener("click", function () {
-    FilterContainer.querySelector(".active").classList.remove("active");
-    this.classList.add("active");
-    const filterValue = this.getAttribute("data-filter");
-    for (let k = 0; k < totalportfolioItem; k++) {
-      if (filterValue === PortfolioItems[k].getAttribute("data-category")) {
-        PortfolioItems[k].classList.remove("hide");
-        PortfolioItems[k].classList.add("show");
-      } else {
-        PortfolioItems[k].classList.remove("show");
-        PortfolioItems[k].classList.add("hide");
-      }
-      if (filterValue === "all") {
-        PortfolioItems[k].classList.remove("hide");
-        PortfolioItems[k].classList.add("show");
-      }
-    }
-  });
-}
-
-const lightbox = document.querySelector(".lightbox"),
-  lightboxImg = lightbox.querySelector(".lightbox-img"),
-  lightboxClose = lightbox.querySelector(".lightbox-close"),
-  lightboxText = lightbox.querySelector(".caption-text"),
-  lightboxCounter = lightbox.querySelector(".caption-counter");
-let itemIndex = 0;
-for (let i = 0; i < totalportfolioItem; i++) {
-  PortfolioItems[i].addEventListener("click", function () {
-    itemIndex = i;
-    changeItem();
-    toggleLightbox();
-  });
-}
-function nextItem() {
-  if (itemIndex == totalportfolioItem - 1) {
-    itemIndex = 0;
-  } else {
-    itemIndex++;
-  }
-  changeItem();
-}
-function prevItem() {
-  if (itemIndex == 0) {
-    itemIndex = totalportfolioItem - 1;
-  } else {
-    itemIndex--;
-  }
-  changeItem();
-}
-function toggleLightbox() {
-  lightbox.classList.toggle("open");
-}
-function changeItem() {
-  imgSrc = PortfolioItems[itemIndex]
-    .querySelector(".portfolio-img img")
-    .getAttribute("src");
-  lightboxImg.src = imgSrc;
-  lightboxText.innerHTML =
-    PortfolioItems[itemIndex].querySelector("h4").innerHTML;
-  lightboxCounter.innerHTML = itemIndex + 1 + " of " + totalportfolioItem;
-}
-
-lightbox.addEventListener("click", function (event) {
-  if (event.target === lightboxClose || event.target === lightbox) {
-    toggleLightbox();
-  }
-});
+		/*====================
+			Google Maps JS
+		======================*/
+		var map = new GMaps({
+				el: '#map',
+				lat: 23.011245,
+				lng: 90.884780,
+				scrollwheel: false,
+			});
+			map.addMarker({
+				lat: 23.011245,
+				lng: 90.884780,
+				title: 'Marker with InfoWindow',
+				infoWindow: {
+				content: '<p>welcome to Medipro</p>'
+			}
+		
+		});
+	});
+	
+	/*====================
+		Preloader JS
+	======================*/
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	});
+	
+	
+})(jQuery);
